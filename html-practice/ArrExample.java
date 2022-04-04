9cf83cd41f82923dfc1afb31f88a09be5357bf36
amarfrontenddeveloper <102942045+amarfrontenddeveloper@users.noreply.github.com>
2022-04-04 10:49:56 +0530

    Add files via upload

---
+++ html-practice/ArrExample.java
@@ -0,0 +1,18 @@
+class ArrExample
+{
+   public static void main(String args[])
+   {
+     
+       int arr[] = {23,45,67,78,89,11};
+       for(int i=0;i<=5;i++)
+       {
+            System.out.println(arr[i]);
+
+       }   
+
+
+   } 
+
+
+
+}
